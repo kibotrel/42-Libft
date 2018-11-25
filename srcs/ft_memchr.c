a4f40d47a3a5cd3e1bc1923a48f4a6afeadc537c
Kilian BOTREL <kibotrel@e3r2p4.42.fr>
2018-11-25 17:18:46 +0100

    NULL protection

--- srcs/ft_memchr.c
+++ srcs/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/11 13:27:08 by kibotrel          #+#    #+#             */
-/*   Updated: 2018/11/14 16:10:42 by kibotrel         ###   ########.fr       */
+/*   Updated: 2018/11/25 17:16:21 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,11 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	unsigned char	*mem;
 
 	mem = (unsigned char*)s;
-	while (n > 0)
+	while (s && --n > 0)
 	{
 		if (*mem == (unsigned char)c)
 			return (mem);
 		mem++;
-		n--;
 	}
 	return (NULL);
 }
