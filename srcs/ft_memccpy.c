f63c2b526c8f0b70d8dba0b342bc9d1ffc7d9bf5
Kilian BOTREL <kibotrel@e3r2p4.42.fr>
2018-11-25 16:41:30 +0100

    Clean up code

--- srcs/ft_memccpy.c
+++ srcs/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/13 11:32:13 by kibotrel          #+#    #+#             */
-/*   Updated: 2018/11/13 12:43:58 by kibotrel         ###   ########.fr       */
+/*   Updated: 2018/11/25 16:40:44 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,12 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 
 	dest = (char*)dst;
 	source = (const unsigned char*)src;
-	i = 0;
-	while (i < n)
+	i = -1;
+	while (++i < n)
 	{
 		dest[i] = source[i];
 		if (source[i] == (unsigned char)c)
 			return ((void*)dest + (i + 1));
-		i++;
 	}
 	return (NULL);
 }
