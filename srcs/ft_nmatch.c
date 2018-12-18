4d3442e12294d5dde657e7875de098fa5714102a
Kilian BOTREL <kibotrel@e2r10p2.42.fr>
2018-12-18 20:32:04 +0100

    Wrong conditional opperator fixed

--- srcs/ft_nmatch.c
+++ srcs/ft_nmatch.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/15 17:19:53 by kibotrel          #+#    #+#             */
-/*   Updated: 2018/11/15 19:44:10 by kibotrel         ###   ########.fr       */
+/*   Updated: 2018/12/18 20:31:09 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_nmatch(char *s1, char *s2)
 	if (s1[0] == '\0' && s2[0] == '\0')
 		return (1);
 	if (s1[0] != '\0' && s2[0] == '*')
-		return (ft_nmatch(s1 + 1, s2) || ft_nmatch(s1, s2 + 1));
+		return (ft_nmatch(s1 + 1, s2) + ft_nmatch(s1, s2 + 1));
 	if (s1[0] == '\0' && s2[0] == '*')
 		return (ft_nmatch(s1, s2 + 1));
 	if (s1[0] == s2[0])
